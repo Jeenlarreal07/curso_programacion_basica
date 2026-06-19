fba45861f3a4db6633bed0df6f2098da87bb8626
jeenlarreal07 <perezlarrealjeenandry@gmail.com>
2026-06-19 13:27:41 -0400

    j

--- Modulo_2/JavaScript/Ejercicios/ejercicio4.js
+++
@@ -1,9 +0,0 @@
-Sistema de Semáforo
-
-Declara una variable `colorSemaforo` y asígnale un valor ("verde", "amarillo" o "rojo").  
-Usando `if`, `else if` y `else`, crea un programa que muestre un mensaje diferente según el color:
-
-- Si es "verde", muestra **"Puede avanzar."**
-- Si es "amarillo", muestra **"Reduzca la velocidad, precaución."**
-- Si es "rojo", muestra **"Debe detenerse."**
-- Si es cualquier otro color, muestra **"Color no válido."**
--- Modulo_3/C#/Ejercicios/ejercicio.cs
+++ Modulo_3/C#/Ejercicios/ejercicio.cs
@@ -95,4 +95,56 @@ class Program
         }
     }
 }
+/*4.  Categoría de Edad: */
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese la edad:");
+        int edad = Convert.ToInt32(Console.ReadLine());
+        VerificarCategoriaEdad(edad);
+    }
 
+    static void VerificarCategoriaEdad(int edad)
+    {
+        if (edad >= 0 && edad <= 12)
+        {
+            Console.WriteLine("La persona es un Niño.");
+        }
+        else if (edad >= 13 && edad <= 17)
+        {
+            Console.WriteLine("La persona es un Adolescente.");
+        }
+        else if (edad >= 18 && edad <= 59)
+        {
+            Console.WriteLine("La persona es un Adulto.");
+        }
+        else if (edad >= 60)
+        {
+            Console.WriteLine("La persona es un Adulto Mayor.");
+        }
+        else
+        {
+            Console.WriteLine("Edad no válida. Por favor ingrese una edad válida.");
+        }
+    }
+}
+/*5.  Tabla de Multiplicar: */
+using System;   
+class Program
+{
+    static void Main(string[] args)
+    {
+        ImprimirTablaMultiplicar(7);
+    }
+
+    static void ImprimirTablaMultiplicar(int numero)
+    {
+        Console.WriteLine($"Tabla de multiplicar del {numero}:");
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine($"{numero} x {i} = {numero * i}");
+        }
+    }
+}
